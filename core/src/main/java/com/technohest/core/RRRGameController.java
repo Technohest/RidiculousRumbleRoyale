f8ed58eddb77545f9be9ff2258ae922e95111513
Tbbas <covernant_4@hotmail.com>
2015-04-22 14:38:43 +0200

    RRRGameController now handles input

--- core/src/main/java/com/technohest/core/RRRGameController.java
+++ core/src/main/java/com/technohest/core/RRRGameController.java
@@ -3,6 +3,8 @@ package com.technohest.core;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector2;
 import com.technohest.assets.characters.Character;
+import com.technohest.constants.Constants;
+import com.technohest.constants.Controls;
 import com.technohest.core.handlers.InputHandler;
 import com.technohest.core.handlers.ScreenHandler;
 
@@ -13,23 +15,15 @@ import static com.technohest.constants.Constants.PPM;
  */
 public class RRRGameController {
     private final RRRGameModel model;
-    private PlayerController playerController;
+    private RRRGameView view;
+    private InputHandler inputHandler;
     public RRRGameController(RRRGameModel model) {
         this.model = model;
-        playerController = new PlayerController(new Character(new Vector2(160/PPM,250/PPM)));
     }
 
     public TiledMap getLevel() {
         return model.getLevel();
     }
-
-    public PlayerController getPlayerController() {
-        return this.playerController;
-    }
-    public void update(float delta) {
-        playerController.update(delta);
-    }
-
 
 
 
@@ -37,8 +31,22 @@ public class RRRGameController {
         if(handler.isPressed(InputHandler.ESCAPE)) {
             ScreenHandler.getInstance().setScreen("menu");
             handler.releaseAllKeys();
-        } else {
-            playerController.handleInput(handler);
+        }
+        if (handler.isPressed(InputHandler.RIGHT)) {
+            model.movePlayer(Controls.RIGHT);
+
+        } if(handler.isPressed(InputHandler.LEFT)) {
+            model.movePlayer(Controls.LEFT);
+
+        }
+        if(handler.isPressed(InputHandler.JUMP)) {
+            model.movePlayer(Controls.UP);
+
+        }
+        if(handler.isPressed(InputHandler.BASE_ATTACK)) {
+            model.movePlayer(Controls.BASE_ATTACK);
         }
     }
+
+
 }
