67343b0d10fce1c7b040992ed1b1f035bbb04b97
oskar367 <oskar.jedvert@gmail.com>
2015-05-07 12:31:22 +0200

    Level now generates using the WorldGenerator

--- core/src/main/java/com/technohest/LibgdxService/levels/MainLevel.java
+++ core/src/main/java/com/technohest/LibgdxService/levels/MainLevel.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.technohest.LibgdxService.ILevel;
+import com.technohest.LibgdxService.WorldGenerator;
 
 import java.util.Collections;
 import java.util.Vector;
@@ -28,7 +29,6 @@ public class MainLevel implements ILevel {
     public MainLevel(){
         TmxMapLoader l = new TmxMapLoader();
         map = l.load("mainlevel.tmx");
-        spawnPoints = new Vector<Vector2>();
         spawnIndex=-1;
     }
     @Override
@@ -39,70 +39,9 @@ public class MainLevel implements ILevel {
     @Override
     public void generate(World world) {
         TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get("Foreground");
-
-        /**
-         * BodyDef and FixtureDef is used as temporary models which can later be added to a body
-         */
-        float tileSize = layer.getTileWidth();
-
-        BodyDef bdef = new BodyDef();
-        bdef.type = BodyDef.BodyType.StaticBody;
-
-        FixtureDef fdef = new FixtureDef();
-        fdef.friction = 0;
-        fdef.filter.categoryBits = 1;
-        fdef.filter.maskBits = -1;
-        fdef.isSensor = false;
-
-        Vector2[] boxVector = new Vector2[5];
-        boxVector[0] = new Vector2(-tileSize/2/PPM, -tileSize/2/PPM);
-        boxVector[1] = new Vector2(-tileSize/2/PPM, tileSize/2/PPM);
-        boxVector[2] = new Vector2(tileSize/2/PPM, tileSize/2/PPM);
-        boxVector[3] = new Vector2(tileSize/2/PPM, -tileSize/2/PPM);
-        boxVector[4] = new Vector2(-tileSize/2/PPM, -tileSize/2/PPM);
-        /**
-         * Create a "Chain Linked" polygon of corners which will check for collision later. Using this chainlink will allow our players to jump up into the box
-         * this can be changed easily by adding more items to the vector, e.g a bottom.
-         */
-        ChainShape cs = new ChainShape();
-        cs.createChain(boxVector);
-        fdef.shape = cs;
-        /**
-         * Create a box2d body for each tile
-         */
-        for(int r = 0; r < layer.getHeight(); r++){
-            for(int c = 0; c < layer.getWidth(); c++){
-                TiledMapTileLayer.Cell cell = layer.getCell(c,r);
-                /**
-                 * If there is a tile on (r,w)
-                 */
-                if(cell != null && cell.getTile() != null){
-                    bdef.position.set(
-                            (c + 0.5f) * tileSize / PPM,
-                            (r + 0.5f) * tileSize / PPM
-                    );
-
-                    world.createBody(bdef).createFixture(fdef);
-                }
-            }
-        }
+        WorldGenerator.generateBox(world, layer);
         layer = (TiledMapTileLayer) map.getLayers().get("Spawnpoints");
-        /**
-         * Create spawnpoints
-         */
-        for(int r = 0; r < layer.getHeight(); r++){
-            for(int c = 0; c < layer.getWidth(); c++){
-                TiledMapTileLayer.Cell cell = layer.getCell(c,r);
-                /**
-                 * If there is a spawnpoint on (r,w) add spawnpoint
-                 */
-                if(cell != null && cell.getTile() != null){
-                    Vector2 point = new Vector2((c + 0.5f) * tileSize / PPM, (r + 0.5f) * tileSize / PPM);
-                    spawnPoints.add(point);
-                }
-            }
-        }
-        Collections.shuffle(spawnPoints);
+        spawnPoints = WorldGenerator.getSpawnPoints(layer);
     }
 
     @Override
