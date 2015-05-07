a2bcef7b4adbc9b139380127921a1963da7e4ac6
Tbbas <covernant_4@hotmail.com>
2015-05-07 15:53:17 +0200

    Added movement/attack methods to interface with javadoc

--- core/src/main/java/com/technohest/LibgdxService/IGameLogic.java
+++ core/src/main/java/com/technohest/LibgdxService/IGameLogic.java
@@ -24,4 +24,25 @@ public interface IGameLogic {
      * @param idCharacterMap
      */
     void generate(ILevel level, HashMap<Integer,Character> idCharacterMap);
+    public void moveLeft(Character player);
+
+    /**
+     * Moves the player(Body with fixtures) left in the game,and plays the run animation.
+     */
+    public void moveRight(Character player);
+
+    /**
+     * Makes the player(Body with fixtures) jump in the game, and plays the jump animation.
+     */
+    public void jump(Character player);
+
+    /**
+     * Makes the character perform a base attack
+     */
+    public void attack_base(Character player);
+
+    /**
+     * Makes the character perform a special attack
+     */
+    public void attack_special(Character player);
 }
