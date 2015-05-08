3d2185605dfaf5c27aba7868270cb0e5e8626164
Tbbas <covernant_4@hotmail.com>
2015-05-08 07:58:04 +0200

    Added temp variable for a temp character(Singleplayer)

--- core/src/main/java/com/technohest/core/model/RRRGameModel.java
+++ core/src/main/java/com/technohest/core/model/RRRGameModel.java
@@ -4,6 +4,8 @@ import com.technohest.LibgdxService.GameLogicGDX;
 import com.technohest.LibgdxService.IGameLogic;
 import com.technohest.LibgdxService.ILevel;
 import com.technohest.core.handlers.LevelHandler;
+
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -20,6 +22,10 @@ public class RRRGameModel {
     public RRRGameModel(){
         this.levelHandler = new LevelHandler();
         setGameLogic(new GameLogicGDX());
+        this.idCharacterMap = new HashMap<Integer, Character>();
+        //Temp character for testing
+        idCharacterMap.put(1,new Character("Allden",new Projectile("FireBall", 100, 10,10),new Projectile("FireBall", 100, 10,10)));
+        myID =1;
     }
     public ILevel getLevel() {
         return levelHandler.getLevel();
@@ -50,6 +56,10 @@ public class RRRGameModel {
         return null;
     }
 
+    public Character getPlayerFromID(Integer myID) {
+        return this.idCharacterMap.get(myID);
+    }
+
     public IGameLogic getGameLogic() {
         return gameLogic;
     }
@@ -60,40 +70,48 @@ public class RRRGameModel {
     public Integer getmyID() {
         return myID;
     }
+    public void setMyID(Integer id) {
+        myID = id;
+    }
 
     /**
-     * Moves the player(Body with fixtures) left in the and plays the run animation.
+     * Moves the player with the specified id left.
      */
     public void moveLeft(Integer playerID){
-
+        gameLogic.moveLeft(getPlayerFromID(playerID));
     }
 
     /**
-     * Moves the player(Body with fixtures) left in the game,and plays the run animation.
+     * Moves the player with the specified id right
      */
     public void moveRight(Integer playerID){
+        gameLogic.moveRight(getPlayerFromID(playerID));
 
     }
 
     /**
-     * Makes the player(Body with fixtures) jump in the game, and plays the jump animation.
+     * Makes the player with the specified id jump
      */
     public void jump(Integer playerID) {
-
+        gameLogic.jump(getPlayerFromID(playerID));
     }
 
     /**
-     * Makes the character perform a base attack
+     * Makes the player with the specified id perform a base attack
      */
     public void attack_base(Integer playerID) {
-
+        gameLogic.attack_base(getPlayerFromID(playerID));
     }
 
     /**
-     * Makes the character perform a special attack
+     * Makes the player with the specified id perform a special attack
      */
     public void attack_special(Integer playerID) {
+        gameLogic.attack_special(getPlayerFromID(playerID));
+    }
 
+    public Collection<Character> getPlayers() {
+        return this.idCharacterMap.values();
     }
 
 
