0e6222cca9bdea07c872452edf2e0dc546f8d39a
Tbbas <covernant_4@hotmail.com>
2015-05-08 07:57:08 +0200

    Added more realistic movement, wrote updateplayer method wich updates State of Characters

--- core/src/main/java/com/technohest/LibgdxService/GameLogicGDX.java
+++ core/src/main/java/com/technohest/LibgdxService/GameLogicGDX.java
@@ -1,5 +1,6 @@
 package com.technohest.LibgdxService;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.technohest.constants.Constants;
@@ -30,6 +31,7 @@ public class GameLogicGDX implements IGameLogic{
     @Override
     public void update(float v){
         world.step(v, 6, 2);
+        updatePlayers(v);
     }
 
     @Override
@@ -43,13 +45,15 @@ public class GameLogicGDX implements IGameLogic{
             i++;
             BodyDef bdef = new BodyDef();
             bdef.gravityScale = 10;
+            bdef.position.set(140,100);
+            bdef.linearVelocity.set(0,0);
+            bdef.type = BodyDef.BodyType.DynamicBody;
             PolygonShape shape = new PolygonShape();
             shape.setAsBox(20/Constants.PPM,30/Constants.PPM);
             FixtureDef fdef = new FixtureDef();
             fdef.shape = shape;
             Body b = world.createBody(bdef);
             b.createFixture(fdef);
-            b.setType(BodyDef.BodyType.DynamicBody);
             bodyCharacterMap.put(b,c);
         }
     }
@@ -64,11 +68,79 @@ public class GameLogicGDX implements IGameLogic{
     }
     public Body getBodyFromCharacter(Character character) {
         for(Map.Entry<Body,Character> e: bodyCharacterMap.entrySet()) {
-            if(e.getValue() == character) {
+            if(e.getValue().equals(character))  {
                 return e.getKey();
             }
         }
         return null;
     }
 
+    /**
+     * Updates the player state and player variables.
+     * @param v
+     */
+    public void updatePlayers(float v) {
+        for (Body b: bodyCharacterMap.keySet()) {
+            if(b.getLinearVelocity().y != 0) {
+                if (b.getLinearVelocity().y < 0) {
+                    Character c = getCharacterfromBody(b);
+                    c.setGrounded(false);
+                    c.setState(Character.State.Falling);
+                } else if(b.getLinearVelocity().y < 0 ) {
+                    Character c = getCharacterfromBody(b);
+                    c.setGrounded(false);
+                    c.setState(Character.State.Jumping);
+                }
+            } else if(b.getLinearVelocity().x != 0) {
+                if(b.getLinearVelocity().x < 0) {
+                    Character c = getCharacterfromBody(b);
+                    c.setGrounded(true);
+                    c.setState(Character.State.Running);
+                    c.setIsFacingRight(false);
+                } else {
+                    Character c = getCharacterfromBody(b);
+                    c.setGrounded(true);
+                    c.setState(Character.State.Running);
+                    c.setIsFacingRight(true);
+                }
+            } else {
+                Character c = getCharacterfromBody(b);
+                c.setGrounded(true);
+                c.setState(Character.State.Standing);
+            }
+        }
+
+    }
+
+    @Override
+    public void moveLeft(Character player){
+        Body playerBody = getBodyFromCharacter(player);
+        playerBody.setLinearVelocity(new Vector2((playerBody.getLinearVelocity().x - Constants.INITIAL_MOVEMENT_SPEED),playerBody.getLinearVelocity().y));
+    }
+
+    @Override
+    public void moveRight(Character player){
+        Body playerBody = getBodyFromCharacter(player);
+        playerBody.setLinearVelocity(new Vector2(playerBody.getLinearVelocity().x + Constants.INITIAL_MOVEMENT_SPEED,playerBody.getLinearVelocity().y));
+
+    }
+
+    @Override
+    public void jump(Character player) {
+        Body playerBody = getBodyFromCharacter(player);
+        playerBody.applyForceToCenter(0,Constants.JUMP_FORCE_MULTIPLIER,true);
+
+    }
+
+    @Override
+    public void attack_base(Character player) {
+        Body playerBody = getBodyFromCharacter(player);
+
+    }
+
+    @Override
+    public void attack_special(Character player) {
+        Body playerBody = getBodyFromCharacter(player);
+
+    }
 }
