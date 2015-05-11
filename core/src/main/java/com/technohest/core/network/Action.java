a09827c8374b37dee0b4563e83d8c1332ba5bec8
oskar367 <oskar.jedvert@gmail.com>
2015-05-11 14:20:50 +0200

    Action now has a timestamp and a ID of the "owner"

--- core/src/main/java/com/technohest/core/network/Action.java
+++ core/src/main/java/com/technohest/core/network/Action.java
@@ -1,17 +1,36 @@
 package com.technohest.core.network;
 
-import com.technohest.core.model.Character;
-
 /**
  * Created by oskar on 2015-05-10.
  */
 public abstract class Action {
-    private Character target;
-    public Character getTarget(){
-        return target;
+    private int id;
+    private int timestamp;
+
+    /**
+     * Id of the original player that made the action
+     * @return
+     * The ID
+     */
+    public int getId(){
+        return id;
     }
-    public void setTarget(Character character){
-        target = character;
+
+    /**
+     * Set the id to the id of the original player
+     * @param a
+     * The ID
+     */
+    public void setId(int a){
+        id = a;
+    }
+
+    public void setTimestamp(int timestamp){
+        this.timestamp = timestamp;
+    }
+
+    public int getTimestamp(){
+        return timestamp;
     }
 
     public abstract void act();
