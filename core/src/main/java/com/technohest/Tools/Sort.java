cac3078d59b4231954e2119ddf08101dfec2af6f
oskar367 <oskar.jedvert@gmail.com>
2015-05-12 14:29:15 +0200

    Added sorting function for timestamp in actions

---
+++ core/src/main/java/com/technohest/Tools/Sort.java
@@ -0,0 +1,28 @@
+package com.technohest.Tools;
+
+import com.technohest.core.network.Action;
+
+import java.util.Vector;
+
+/**
+ * Created by vilddjur on 2015-05-12.
+ */
+public class Sort {
+    public static Vector<Action> sortTime(Vector<Action> vector){
+        boolean done = false;
+        Action temp;
+        while (!done){
+            done = true;
+            for(int i = 0;i<vector.size();i++){
+                if(vector.get(i).getTimestamp() < vector.get(i+1).getTimestamp()){
+                    temp = vector.get(i);
+                    vector.set(i, vector.get(i+1));
+                    vector.set(i+1, temp);
+                    done = false;
+                }
+            }
+        }
+
+        return vector;
+    }
+}
