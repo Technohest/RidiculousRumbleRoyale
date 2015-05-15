f3fa82b9aacb429234069da69516729140cd8c93
oskar367 <oskar.jedvert@gmail.com>
2015-05-15 12:00:06 +0200

    Made StateGDX a singleton

--- core/src/main/java/com/technohest/core/network/StateGDX.java
+++ core/src/main/java/com/technohest/core/network/StateGDX.java
@@ -11,8 +11,16 @@ import java.util.HashMap;
 public class StateGDX implements IState {
     private HashMap<Character, Vector2> map;
 
-    public StateGDX(){
+    private static StateGDX     instance = null;
+    protected StateGDX(){
         map = new HashMap<Character, Vector2>();
+    }
+
+    public StateGDX getInstance(){
+        if(instance == null){
+            instance = new StateGDX();
+        }
+        return instance;
     }
 
     @Override
@@ -28,5 +36,4 @@ public class StateGDX implements IState {
     public boolean equals(HashMap<Character, Vector2> map){
         return this.map.equals(map);
     }
-
 }
