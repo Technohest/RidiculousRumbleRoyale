4f989b5a92cec2cfda070b4bdb7da8074f5b9c4f
oskar367 <oskar.jedvert@gmail.com>
2015-05-18 13:56:56 +0200

    moved for the last time guys sry

---
+++ core/test/java/com/technohest/test/GameLogicGDXTest.java
@@ -0,0 +1,16 @@
+package com.technohest.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vilddjur on 2015-05-18.
+ */
+public class GameLogicGDXTest {
+
+    @Test
+    public void testCorrect() throws Exception {
+
+    }
+}
