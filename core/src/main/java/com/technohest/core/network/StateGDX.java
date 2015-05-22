0d472a876988e4d01d6e85ba257785c6492b200e
Tbbas <covernant_4@hotmail.com>
2015-05-22 16:04:52 +0200

    Added attackBodymap

--- core/src/main/java/com/technohest/core/network/StateGDX.java
+++ core/src/main/java/com/technohest/core/network/StateGDX.java
@@ -1,6 +1,8 @@
 package com.technohest.core.network;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.technohest.core.model.Attack;
 import com.technohest.core.model.Character;
 
 import java.util.ArrayList;
@@ -11,10 +13,12 @@ import java.util.HashMap;
  */
 public class StateGDX implements IState {
     private HashMap<Character, ArrayList<Vector2>> map;
+    private HashMap<Attack,Body> attackBodyMap;
 
     private static StateGDX     instance = null;
     protected StateGDX(){
         map = new HashMap<Character, ArrayList<Vector2>>();
+        attackBodyMap = new HashMap<Attack, Body>();
     }
 
     public static StateGDX getInstance(){
@@ -25,17 +29,22 @@ public class StateGDX implements IState {
     }
 
     @Override
-    public HashMap<Character, ArrayList<Vector2>> getState() {
+    public HashMap<Character, ArrayList<Vector2>> getCharacterStates() {
         return map;
     }
-
     @Override
-    public void setState(HashMap<Character, ArrayList<Vector2>> map) {
-        this.map = map;
+    public HashMap<Attack,Body> getAttackStates() {
+        return attackBodyMap;
     }
 
     @Override
-    public boolean equals(HashMap<Character, ArrayList<Vector2>> map){
-        return this.map.equals(map);
+    public void setState(HashMap<Character, ArrayList<Vector2>> map,HashMap<Attack,Body> bodyAttackMap) {
+        this.map = map;
+        this.attackBodyMap = bodyAttackMap;
+    }
+
+    @Override
+    public boolean equals(HashMap<Character, ArrayList<Vector2>> map, HashMap<Attack,Body> attackBodyMap){
+        return this.map.equals(map) && attackBodyMap.equals(this.attackBodyMap);
     }
 }
