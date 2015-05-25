fdf1dd7548f87fdccf0021ed2c17f111616aead5
Tbbas <covernant_4@hotmail.com>
2015-05-25 10:33:11 +0200

    Removed Character references, switched to integers

--- core/src/main/java/com/technohest/LibgdxService/GameLogicGDX.java
+++ core/src/main/java/com/technohest/LibgdxService/GameLogicGDX.java
@@ -1,9 +1,7 @@
 package com.technohest.LibgdxService;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.esotericsoftware.minlog.Log;
 import com.technohest.constants.Constants;
 import com.technohest.core.model.*;
 import com.technohest.core.model.Character;
@@ -12,7 +10,7 @@ import com.technohest.core.network.StateGDX;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Set;
 
 /**
  * The service managing the game physics.
@@ -21,21 +19,17 @@ import java.util.Iterator;
  */
 public class GameLogicGDX implements IGameLogic {
     private final World world;
-    private HashMap<Attack,Body> attackBodyMap;
+    private HashMap<Integer,Body> attackIdBodyMap;
     CollisionHandler collisionHandler;
-    RRRGameModel model;
-    //A map for bundling bodies with player objects
-    private HashMap<Character, Body> characterBodyMap;
-
-    //THIS ONE SHOULD BE IN MODEL!
-    private HashMap<Integer,Character> idCharacterMap;
+    RRRGameModel model;    //THIS ONE SHOULD BE IN MODEL!
+    private HashMap<Integer,Body> characterIdBodyMap;
 
     public GameLogicGDX(RRRGameModel model){
         world = new World(new Vector2(0, Constants.GRAVITY), true);
-        characterBodyMap = new HashMap<Character,Body>();
         collisionHandler = new CollisionHandler(this);
         world.setContactListener(collisionHandler);
-        this.attackBodyMap = new HashMap<Attack, Body>();
+        this.attackIdBodyMap = new HashMap<Integer, Body>();
+        this.characterIdBodyMap = new HashMap<Integer, Body>();
         this.model = model;
     }
 
@@ -50,16 +44,14 @@ public class GameLogicGDX implements IGameLogic {
     }
 
     @Override
-    public void generate(ILevel level,HashMap<Integer,Character> idCharacterMap) {
-        this.idCharacterMap = idCharacterMap;
+    public void generate(ILevel level,ArrayList<Integer> characterIdArray) {
         level.generate(world);
-        for (Character c: idCharacterMap.values()) {
-            //Move to model
-            createPlayer(c);
+        for (Integer i: characterIdArray) {
+            createPlayer(i);
         }
     }
 
-    public void createPlayer(Character character) {
+    public void createPlayer(Integer playerId) {
         BodyDef bdef = new BodyDef();
         bdef.type = BodyDef.BodyType.DynamicBody;
         bdef.gravityScale = 5;
@@ -70,11 +62,11 @@ public class GameLogicGDX implements IGameLogic {
         fdef.shape = shape;
         Body b = world.createBody(bdef);
         b.setLinearDamping(10);
-        b.createFixture(fdef).setUserData(character);
+        b.createFixture(fdef).setUserData(playerId);
         shape.setAsBox(4 / 32.0f, 4 / 32.0f, new Vector2(0, -((Constants.PLAYER_HEIGHT) / 32.0f)), 0);
         fdef.isSensor = true;
         b.createFixture(fdef).setUserData(b);
-        characterBodyMap.put(character,b);
+        characterIdBodyMap.put(playerId, b);
 
     }
 
@@ -83,100 +75,103 @@ public class GameLogicGDX implements IGameLogic {
      * @param body the body which will be used to search for the character.
      * @return the corresponding character to a body or null if the character was not found.
      */
-    public Character getCharacterfromBody(Body body) {
-        if(characterBodyMap.containsValue(body)) {
-            for (Character c : characterBodyMap.keySet()) {
-                if (characterBodyMap.get(c).equals(body)) {
-                    return c;
+    public Integer getCharacterIdfromBody(Body body) {
+        if(characterIdBodyMap.containsValue(body)) {
+            for (Integer i : characterIdBodyMap.keySet()) {
+                if (characterIdBodyMap.get(i).equals(body)) {
+                    return i;
                 }
             }
         }
         return null;
     }
-    public Body getBodyFromCharacter(Character character) {
-        return characterBodyMap.get(character);
+    public Body getBodyFromplayerId(Integer id) {
+        return characterIdBodyMap.get(id);
     }
 
-//Perhaps move state setting to model
-    public void updatePlayer(Character c) {
-        Body b = getBodyFromCharacter(c);
+    /**
+     * Returns an Integer specifying state of character, 0 = falling, 1 = jumping, 2 = runnning right, 3 = running left, 4 = standing.
+     * @param playerId
+     * @return
+     */
+    public Integer getStateOfPlayer(Integer playerId) {
+        Body b = getBodyFromplayerId(playerId);
         if (b.getLinearVelocity().y != 0) {
-            if (b.getLinearVelocity().y < 0) {
-                c.setState(Character.State.Falling);
-            } else if (b.getLinearVelocity().y > 0) {
-                c.setState(Character.State.Jumping);
+            if (b.getLinearVelocity().y > 0) {
+                return 1;
+            } else {
+                return 0;
             }
         } else if (b.getLinearVelocity().x != 0) {
-            if (b.getLinearVelocity().x < 0) {
-                c.setState(Character.State.Running);
-                c.setIsFacingRight(false);
+            if (b.getLinearVelocity().x > 0) {
+                return 2;
             } else {
-                c.setState(Character.State.Running);
-                c.setIsFacingRight(true);
+                return 3;
             }
         } else {
-            c.setState(Character.State.Standing);
+            return 4;
         }
     }
 
-    public Body getBodyFromAttack(Attack attack) {
-       return attackBodyMap.get(attack);
+
+
+    public Body getBodyFromAttackId(Integer attackId) {
+       return attackIdBodyMap.get(attackId);
     }
 
 
-
-    public void respawnPlayer(Character player) {
-                createPlayer(player);
+    @Override
+    public void respawnPlayer(Integer playerId) {
+        createPlayer(playerId);
     }
 
-
-    public void destroyAttack(Attack attack) {
-        Body b = getBodyFromAttack(attack);
+    @Override
+    public void destroyAttack(Integer attackId) {
+        Body b = getBodyFromAttackId(attackId);
         if(b != null) {
             world.destroyBody(b);
         }
     }
 
 
-
-    public void killPlayer(Character character) {
-        world.destroyBody(getBodyFromCharacter(character));
-        characterBodyMap.remove(character);
+    @Override
+    public void killPlayer(Integer characterId) {
+        world.destroyBody(getBodyFromplayerId(characterId));
+        characterIdBodyMap.remove(characterId);
     }
 
 
 
 
     @Override
-    public void moveLeft(Character player){
-        Body playerBody = getBodyFromCharacter(player);
+    public void moveLeft(Integer playerId){
+        Body playerBody = getBodyFromplayerId(playerId);
         playerBody.setLinearVelocity(new Vector2(-Constants.INITIAL_MOVEMENT_SPEED, playerBody.getLinearVelocity().y));
     }
 
     @Override
-    public void moveRight(Character player){
-        Body playerBody = getBodyFromCharacter(player);
+    public void moveRight(Integer playerId){
+        Body playerBody = getBodyFromplayerId(playerId);
         playerBody.setLinearVelocity(new Vector2(Constants.INITIAL_MOVEMENT_SPEED,playerBody.getLinearVelocity().y));
 
     }
 
     @Override
-    public void jump(Character player) {
-        if(player.isGrounded()) {
-            Body playerBody = getBodyFromCharacter(player);
+    public void jump(Integer playerId) {
+        if(getStateOfPlayer(playerId) != 0 || getStateOfPlayer(playerId) != 1) {
+            Body playerBody = getBodyFromplayerId(playerId);
             playerBody.applyForceToCenter(0, Constants.JUMP_FORCE_MULTIPLIER, true);
-            player.setGrounded(false);
         }
 
     }
 
     @Override
-    public void attack_base(Character player) {
-        Body playerBody = getBodyFromId(player.getId());
+    public void attack_base(Integer playerId,boolean isFacingRight) {
+        Body playerBody = getBodyFromplayerId(playerId);
             BodyDef bdef = new BodyDef();
             bdef.type = BodyDef.BodyType.DynamicBody;
             bdef.gravityScale = 0;
-            if (player.isFacingRight()) {
+            if (isFacingRight) {
                 bdef.position.set((playerBody.getPosition().x + Constants.PLAYER_WIDTH/32.0f), (playerBody.getPosition().y));
             } else {
                 bdef.position.set((playerBody.getPosition().x - Constants.PLAYER_WIDTH/32.0f), (playerBody.getPosition().y));
@@ -187,27 +182,25 @@ public class GameLogicGDX implements IGameLogic {
             fdef1.shape = shape;
             Body b = world.createBody(bdef);
             b.setLinearDamping(0);
-            b.setUserData(player.getBaseAttack());
-            b.createFixture(fdef1).setUserData(player.getBaseAttack());
-            attackBodyMap.put(player.getBaseAttack(),b);
+            b.createFixture(fdef1).setUserData(playerId);
+            attackIdBodyMap.put(playerId,b);
     }
 
 
     @Override
-    public void attack_special(Character player) {
-        Body playerBody = getBodyFromId(player.getId());
+    public void attack_special(Integer playerId,boolean isFacingRight) {
+        Body playerBody = getBodyFromplayerId(playerId);
             BodyDef bdef = new BodyDef();
             bdef.type = BodyDef.BodyType.DynamicBody;
             bdef.gravityScale = 0;
-            if (player.isFacingRight()) {
+            if (isFacingRight) {
                 bdef.position.set((playerBody.getPosition().x + Constants.PLAYER_WIDTH/32.0f), (playerBody.getPosition().y));
-                bdef.linearVelocity.set(((Projectile)player.getSpecialAttack()).getVelocityX(),0);
+                bdef.linearVelocity.set(10,0);
 
             } else {
                 bdef.position.set((playerBody.getPosition().x - Constants.PLAYER_WIDTH / 32.0f), (playerBody.getPosition().y));
-                bdef.linearVelocity.set(-((Projectile)player.getSpecialAttack()).getVelocityX(), 0);
+                bdef.linearVelocity.set(-10, 0);
             }
-
 
             FixtureDef fdef1 = new FixtureDef();
             PolygonShape shape = new PolygonShape();
@@ -215,11 +208,11 @@ public class GameLogicGDX implements IGameLogic {
             fdef1.shape = shape;
             Body b = world.createBody(bdef);
             b.setLinearDamping(0);
-            b.setUserData(player.getSpecialAttack());
-            b.createFixture(fdef1).setUserData(player.getSpecialAttack());
-            attackBodyMap.put(player.getSpecialAttack(),b);
+            b.createFixture(fdef1).setUserData(playerId);
+            attackIdBodyMap.put(playerId,b);
     }
 
+    /*
         public Character getCharacterFromAttack(Attack attack) {
             if(attack instanceof Projectile) {
                 for(Character c:getCharacters()) {
@@ -237,14 +230,14 @@ public class GameLogicGDX implements IGameLogic {
                 return null;
 
         }
+        */
 
 
 
     @Override
     public void setCharacterState(Integer playerId, Vector2 pos, Vector2 vel) {
         //Body playerBody = getBodyFromCharacter(player);
-        Body playerBody = getBodyFromId(playerId);
-
+        Body playerBody = getBodyFromplayerId(playerId);
         if (playerBody != null) {
             playerBody.setLinearVelocity(vel);
             playerBody.setTransform(pos, playerBody.getAngle());
@@ -252,77 +245,73 @@ public class GameLogicGDX implements IGameLogic {
     }
 
     @Override
-    public void setAttackState(Attack attack, Vector2 position, Vector2 velocity) {
-        Body attackBody = getBodyFromAttack(attack);
-
+    public void setAttackState(Integer attackId, Vector2 position, Vector2 velocity) {
+        Body attackBody = getBodyFromAttackId(attackId);
         if (attackBody != null) {
             attackBody.setLinearVelocity(velocity);
             attackBody.setTransform(position, attackBody.getAngle());
         }
         else {
-            if (getCharacterFromAttack(attack) != null) {
-                if (attack instanceof MeleeAttack) {
-                    attack_base(getCharacterFromAttack(attack));
-                } else {
-                    attack_special(getCharacterFromAttack(attack));
-                }
+            BodyDef bdef = new BodyDef();
+            bdef.type = BodyDef.BodyType.DynamicBody;
+            bdef.gravityScale = 0;
+            bdef.position.set(position);
+            bdef.linearVelocity.set(velocity);
+            FixtureDef fdef1 = new FixtureDef();
+            PolygonShape shape = new PolygonShape();
+            if(velocity.x == 0) {
+                shape.setAsBox(5.0f / 32.0f, 8.0f / 32.0f);
+            }else {
+                shape.setAsBox(10.0f / 32.0f, 10.0f / 32.0f);
             }
-        }
-    }
+            fdef1.shape = shape;
+            Body b = world.createBody(bdef);
+            b.setLinearDamping(0);
+            b.createFixture(fdef1).setUserData(attackId);
+            attackIdBodyMap.put(attackId,b);
 
-    private Body getBodyFromId(Integer playerId) {
-        return characterBodyMap.get(idCharacterMap.get(playerId));
+        }
     }
 
     @Override
     public void correct(IState newState) {
-        HashMap<Character, ArrayList<Vector2>> map =  newState.getCharacterStates();
-        for(Character c : map.keySet()){
-            ArrayList<Vector2> temp = map.get(c);
-            setCharacterState(c.getId(), temp.get(0), temp.get(1));
+        HashMap<Integer, ArrayList<Vector2>> map =  newState.getCharacterIdStates();
+        for(Integer i : map.keySet()){
+            ArrayList<Vector2> temp = map.get(i);
+            setCharacterState(i, temp.get(0), temp.get(1));
         }
-        HashMap<Attack,ArrayList<Vector2>> attackVectorMap = newState.getAttackStates();
+        HashMap<Integer,ArrayList<Vector2>> attackVectorMap = newState.getAttackIdStates();
         if(!attackVectorMap.isEmpty()) {
-            for (Attack a : attackVectorMap.keySet()) {
-                ArrayList<Vector2> temp = attackVectorMap.get(a);
-                setAttackState(a, temp.get(0), temp.get(1));
+            for (Integer i : attackVectorMap.keySet()) {
+                ArrayList<Vector2> temp = attackVectorMap.get(i);
+                setAttackState(i, temp.get(0), temp.get(1));
             }
         }
-        model.setEnabledAttacks(attackBodyMap.keySet());
     }
 
     @Override
-    public void generateState() {
+    public void generateState(Set<Character> aliveCharacters, Set<Attack> activeAttacks) {
         HashMap<Character, ArrayList<Vector2>> map = new HashMap<Character, ArrayList<Vector2>>();
-        Collection<Character> chars = getCharacters();
-        for(Character c : chars){
+        for(Character c:aliveCharacters){
             ArrayList<Vector2> vector2s = new ArrayList<Vector2>();
-            Body playerBody = getBodyFromCharacter(c);
+            Body playerBody = getBodyFromplayerId(c.getId());
             vector2s.add(playerBody.getPosition());
             vector2s.add(playerBody.getLinearVelocity());
-
             map.put(c, vector2s);
         }
         HashMap<Attack,ArrayList<Vector2>> attackVectorMap = new HashMap<Attack, ArrayList<Vector2>>();
-        for(Attack a:attackBodyMap.keySet()) {
-            if(a.isReady()) {
+        for(Attack a:activeAttacks) {
                 ArrayList<Vector2> Vector2s = new ArrayList<Vector2>();
-                Vector2s.add(attackBodyMap.get(a).getPosition());
-                Vector2s.add(attackBodyMap.get(a).getLinearVelocity());
+                Vector2s.add(attackIdBodyMap.get(a.getSourcePlayerId()).getPosition());
+                Vector2s.add(attackIdBodyMap.get(a.getSourcePlayerId()).getLinearVelocity());
                 attackVectorMap.put(a, Vector2s);
-            }
+
         }
         StateGDX state = StateGDX.getInstance();
         state.setState(map,attackVectorMap);
     }
 
-    public Collection<Character> getCharacters() {
-        Collection<Character> aliveCharacters = new ArrayList<Character>();
-        for(Character c:idCharacterMap.values()) {
-            if(c.isAlive()) {
-                aliveCharacters.add(c);
-            }
-        }
-        return aliveCharacters;
+    public Collection<Integer> getCharacterIds() {
+        return characterIdBodyMap.keySet();
     }
 }
